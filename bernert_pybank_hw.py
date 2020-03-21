65f359d55798c6ba06887e3926edb29e856bc51d
Paul Bernert <pwberner@asu.edu>
2020-03-21 14:34:28 -0700

    +write -print

--- bernert_pybank_hw.py
+++ bernert_pybank_hw.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df = pd.read_csv("PyBank/Resources/budget_data.csv")
 
-months = len(df)
+months = len(df.Date.unique())
 total = df["Profit/Losses"].sum()
 average = np.diff([df["Profit/Losses"]]).mean()
 
@@ -15,10 +15,10 @@ minindex = df["Profit/Losses"].idxmin()
 mindate = df["Date"][minindex]
 minprofit = df["Profit/Losses"][minindex]
 
-print("Financial Analysis")
-print("-------------------------")
-print(f"Total Months: {months}")
-print(f"Total: ${total}")
-print(f"Average Change: {average}")
-print(f"Greatest Increase in Profits: {maxdate} ${maxprofit}")
-print(f"Greatest Decrease in Profits: {mindate} ${minprofit}")
+with open("PyBank/bernert_pybank_output.csv",'w') as f:
+    f.write("Financial Analysis\n")
+    f.write("-------------------------\n")
+    f.write(f"Total Months: {months}\n")
+    f.write(f"Average Change: {average}\n")
+    f.write(f"Greatest Increase in Profits: {maxdate} ${maxprofit}\n")
+    f.write(f"Greatest Decrease in Profits: {mindate} ${minprofit}\n")
--- bernert_pypoll_hw.py
+++ bernert_pypoll_hw.py
@@ -9,12 +9,13 @@ x = df.Candidate.unique()
 z = df["Candidate"].value_counts()
 y = ((z / tot_votes) * 100).round(2)
 
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {tot_votes}")
-print("-------------------------")
-for i in range(4):
-    print(f"{x[i]}: {y[i]}% ({z[i]})")
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
+with open("PyPoll/bernert_pypoll_output.csv",'w') as f:
+    f.write("Election Results\n")
+    f.write("-------------------------\n")
+    f.write(f"Total Votes: {tot_votes}\n")
+    f.write("-------------------------\n")
+    for i in range(4):
+        f.write(f"{x[i]}: {y[i]}% ({z[i]})\n")
+    f.write("-------------------------\n")
+    f.write(f"Winner: {winner}\n")
+    f.write("-------------------------\n")
